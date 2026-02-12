be742b3aef165f1f4b9730a99c7617175482c3ef
Patou <75997617+xorblo-doitus@users.noreply.github.com>
2026-02-12 20:41:12 +0100

    Check diagonal winning and show winning line

--- tic-tac-toe/src/App.tsx
+++ tic-tac-toe/src/App.tsx
@@ -1,50 +1,64 @@
 import { useState, type ReactNode } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
 import './App.css'
 
 
-function playerToMark(value: number): string {
-	switch (value) {
-		case 0: return "  ";
-		case -1: return "╳";
-		case 1: return "◯";
+// An enum would be better, but I don't know if using non-erasable TypeScript
+// is a good pratice.
+type Player = -1|0|1;
+const PLAYER_LEFT: Player = -1;
+const PLAYER_NONE: Player = 0;
+const PLAYER_RIGHT: Player = 1;
+
+type Position = {
+	x: number;
+	y: number;
+};
+
+function arePositionsEqual(a: Position, b: Position): boolean {
+	return a.x == b.x && a.y == b.y
+}
+
+
+function playerToMark(player: Player): string {
+	switch (player) {
+		case PLAYER_NONE: return "  ";
+		case PLAYER_LEFT: return "╳";
+		case PLAYER_RIGHT: return "◯";
 		default: return "?";
 	}
 }
 
-function playerToClass(player: number): string {
+function playerToClass(player: Player): string {
 	switch (player) {
-		case -1: return "left-player";
-		case 1: return "right-player";
+		case PLAYER_LEFT: return "left-player";
+		case PLAYER_RIGHT: return "right-player";
 		default: return "no-player";
 	}
 }
 
-function playerToText(player: number): string {
+function playerToText(player: Player): string {
 	switch (player) {
-		case -1: return "joueur de gauche";
-		case 1: return "joueur de droite";
+		case PLAYER_LEFT: return "joueur de gauche";
+		case PLAYER_RIGHT: return "joueur de droite";
 		default: return "[erreur : joueur inconnu]";
 	}
 }
 
 
-function Square({value, clickCallback}: {value: number, clickCallback: React.MouseEventHandler<HTMLButtonElement>}) {
-	
+function Square({player, inWinningLine, clickCallback}: {player: Player, inWinningLine: boolean, clickCallback: React.MouseEventHandler<HTMLButtonElement>}) {
 	return <>
 		<button
-			className="square"
+			className={"square" + (inWinningLine ? " winning" : "")}
 			onClick={clickCallback}
 		>
-			{playerToMark(value)}
+			{playerToMark(player)}
 		</button>
 	</>
 }
 
 
-function PlayerMessage({player}: {player: number}) {
-	if (player == 0) {
+function PlayerMessage({player}: {player: Player}) {
+	if (player === PLAYER_NONE) {
 		return <></>;
 	}
 	return <>
@@ -55,13 +69,17 @@ function PlayerMessage({player}: {player: number}) {
 }
 
 
-function WinnerMessage({winner}: {winner: number}) {
-	if (winner == 0) {
+function WinnerMessage({winner}: {winner: Player|null}) {
+	if (winner === null) {
 		return <></>;
 	}
 	return <>
 		<div className="winner-msg">
-			<p>Le {playerToText(winner)} a gagné !</p>
+			{
+			winner === PLAYER_NONE
+			? <p>Match nul...</p>
+			: <p>Le {playerToText(winner)} a gagné !</p>
+			}
 			<p>Cliquez sur recommencer pour jouer à nouveau.</p>
 		</div>
 	</>
@@ -70,14 +88,16 @@ function WinnerMessage({winner}: {winner: number}) {
 
 
 function Board({nbColumns, nbRows}: {nbColumns: number, nbRows: number}) {
-	const [board, setBoard] = useState(createBoard());
-	const [player, setPlayer] = useState(1);
-	const winner = checkWinner();
+	const winLength = 3;
 	
-	function createBoard(): number[][] {
-		const board: number[][] = [];
+	const [board, setBoard] = useState(createBoard());
+	const [player, setPlayer] = useState(PLAYER_LEFT);
+	const [winner, winningLine]: [Player|null, Position[]] = checkWinner();
+	
+	function createBoard(): Player[][] {
+		const board: Player[][] = [];
 		for (let x = 0; x < nbColumns; x++) {
-		const column: number[] = []
+		const column: Player[] = []
 			for (let y = 0; y < nbRows; y++) {
 				column.push(0);
 			}
@@ -86,34 +106,127 @@ function Board({nbColumns, nbRows}: {nbColumns: number, nbRows: number}) {
 		return board;
 	}
 	
-	function checkWinner(): number {
-		// TODO should return 0 for draw and null for ongoing game instead
-		
-		for (const row of board) {
-			if (Math.abs(row.reduce((a,b)=>a+b)) == nbColumns) {
-				return row[0];
-			}
-		}
-		
-		for (let y = 0; y < nbColumns; y++) {
-			if (Math.abs(board.reduce((sum, row)=>sum+row[y], 0)) == nbRows) {
-				return board[y][0];
-			}
-		}
-		
-		// TODO check diagonals
-		
-		return 0;
+	
+	function getPlayerMarkAt(position: Position) {
+		return board[position.x][position.y];
 	}
 	
-	function onSquareClicked(x: number, y: number) {
-		if (board[x][y] != 0 || checkWinner() != 0) {
+	
+	function checkWinnerOnLine(positions: Position[]): [Player|null, Position[]] {
+		let streak: Position[] = []
+		
+		for (const position of positions) {
+			const player = getPlayerMarkAt(position);
+			
+			if (player != PLAYER_NONE && (streak.length === 0 || player === getPlayerMarkAt(streak.at(-1)!))) {
+				streak.push(position)
+			} else {
+				streak = [];
+			}
+			
+			if (streak.length >= winLength) {
+				break;
+			}
+		}
+		
+		if (streak.length >= winLength) {
+			return [getPlayerMarkAt(streak[0]), streak];
+		}
+		
+		return [null, []];
+	}
+	
+	
+	function* getVerticalLines(): Generator<Position[], void, void> {
+		for (let y = 0; y < nbColumns; y++) {
+			const line: Position[] = [];
+			
+			for (let x = 0; x < nbRows; x++) {
+				line.push({x: x, y: y});
+			}
+			
+			yield line;
+		}
+	}
+	
+	
+	function* getHorizontalLines(): Generator<Position[], void, void> {
+		for (let x = 0; x < nbColumns; x++) {
+			const line: Position[] = [];
+			
+			for (let y = 0; y < nbRows; y++) {
+				line.push({x: x, y: y});
+			}
+			
+			yield line;
+		}
+	}
+	
+	function* getDiagonalLines(): Generator<Position[], void, void> {
+		for (let startX = 0; startX <= nbColumns - winLength; startX++) {
+			for (let startY = 0; startY <= nbRows - winLength; startY++) {
+				const stop = Math.min(nbColumns - startX, nbRows - startY);
+				
+				const descendingDiagonal: Position[] = [];
+				for (let offset = 0; offset < stop; offset++) {
+					descendingDiagonal.push({x: startX + offset, y: startY + offset});
+				}
+				yield descendingDiagonal;
+				
+				const reverseStartY = nbRows - startY - 1;
+				const ascendingDiagonal: Position[] = [];
+				for (let offset = 0; offset < stop; offset++) {
+					ascendingDiagonal.push({x: startX + offset, y: reverseStartY - offset});
+				}
+				yield ascendingDiagonal;
+			}
+		}
+	}
+	
+	
+	function* getAllLines(): Generator<Position[], void, void> {
+		for (const line of getVerticalLines()) {
+			yield line;
+		}
+		for (const line of getHorizontalLines()) {
+			yield line;
+		}
+		for (const line of getDiagonalLines()) {
+			yield line;
+		}
+	}
+	
+	
+	function checkWinner(): [Player|null, Position[]] {
+		// Winning
+		for (const line of getAllLines()) {
+			const [winner, winningPositions] = checkWinnerOnLine(line);
+			
+			if (winner !== null) {
+				return [winner, winningPositions];
+			}
+		}
+		
+		
+		// Draw
+		if (board.every(column=>column.every(player => player != PLAYER_NONE))) {
+			return [PLAYER_NONE, []];
+		}
+		
+		// Game continuing
+		return [null, []];
+	}
+	
+	function onSquareClicked(position: Position) {
+		const {x, y} = position;
+		
+		if (board[x][y] !== PLAYER_NONE || checkWinner()[0] !== null) {
 			return;
 		}
 		
 		const newBoard = board.slice(); // copy
 		newBoard[x][y] = player;
-		setPlayer(-player);
+		setPlayer(-player as Player);
 		setBoard(newBoard);
 	}
 	
@@ -125,23 +238,26 @@ function Board({nbColumns, nbRows}: {nbColumns: number, nbRows: number}) {
 	const squares: ReactNode[] = []
 	for (let x = 0; x < nbColumns; x++) {
 		for (let y = 0; y < nbRows; y++) {
+			const position: Position = {x: x, y: y};
+			
 			squares.push(<Square
-				value={board[x][y]}
-				clickCallback={() => onSquareClicked(x, y)}
+				player={board[x][y]}
+				inWinningLine={winningLine.some(winningPosition => arePositionsEqual(winningPosition, position))}
+				clickCallback={() => onSquareClicked(position)}
 				key={x*nbColumns+y}
 			/>);
 		}
 	}
 	
-	if (nbColumns == 0 || nbRows == 0) {
+	if (nbColumns === 0 || nbRows === 0) {
 		return <>
-			<div className="error">Erreur : Le plateau ne peut avoir une dimension à 0.</div>
+			<div className="error">Erreur : Le plateau ne peut pas avoir une dimension à 0.</div>
 		</>
 	}
 	
 	return <>
-		<PlayerMessage player={player} />
-		<div className="squares">
+		{winner === null ? <PlayerMessage player={player} /> : null}
+		<div className="squares" style={{gridTemplateColumns: `repeat(${nbColumns}, auto)`}}>
 			{squares}
 		</div>
 		<WinnerMessage winner={winner} />
@@ -149,67 +265,9 @@ function Board({nbColumns, nbRows}: {nbColumns: number, nbRows: number}) {
 	</>
 }
 
-// const NB_COLUMNS = 3; // note : CSS column count is hardcoded
-// const NB_ROWS = 3;
-// // const [board] = useState([[null]])
-
-
-// function createBoard(): number[][] {
-// 	const board: number[][] = [];
-// 	for (let x = 0; x < NB_COLUMNS; x++) {
-// 	const column: number[] = []
-// 		for (let y = 0; y < NB_ROWS; y++) {
-// 			column.push(0);
-// 		}
-// 		board.push(column);
-// 	}
-// 	return board;
-// }
-
-
-// function Square({board, x, y}: {board: number[][], x: number, y: number}) {
-// 	function handleClick() {
-// 		const newBoard = board.slice(); // copy
-// 		newBoard[x][y] = 1;
-// 		setBoard(newBoard);
-// 	}
-	
-// 	return <>
-// 		<button
-// 			className="square"
-// 			onClick={handleClick}
-// 		>
-// 			x: {x}, y: {y}
-// 			<strong>value: {BOARD[x][y]}</strong>
-// 		</button>
-// 	</>
-// }
-
-
-
-// function Board() {
-// 	const [BOARD, setBoard] = useState(createBoard());
-	
-// 	const squares: ReactNode[] = []
-// 	for (let x = 0; x < NB_COLUMNS; x++) {
-// 		for (let y = 0; y < NB_ROWS; y++) {
-// 			squares.push(<Square x={x} y={y} />)
-// 		}
-// 	}
-	
-// 	return <>
-// 		<div className="board">
-// 			{squares}
-// 		</div>
-// 	</>
-// }
-
-
 
 
 function App() {
-	// const [count, setCount] = useState(0)
-
 	return (
 		<>
 			<Board nbColumns={3} nbRows={3} />
